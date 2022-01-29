219a834dd301cd13fc0c348635534b7a5bbde232
ViVidam <1325507519@qq.com>
2022-01-29 00:44:37 -0800

    layer2

--- disk.h
+++ disk.h
@@ -2,11 +2,6 @@
 #define BLOCKSIZE  4096 //4K
 #define BLOCKCOUNT  100
 
-/*typedef struct
-{
-	uint8_t data[BLOCKSIZE];	
-} block;*/
-
 static uint8_t dev[BLOCKCOUNT][BLOCKSIZE];
 int write_disk(unsigned int block_id,void* buffer);
 int read_disk(unsigned int block_id,void* buffer);

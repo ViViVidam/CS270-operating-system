6dbd0aa2077147ca2eb3f89ae021a3f583eae6b1
ViVidam <1325507519@qq.com>
2022-03-07 16:26:31 -0800

    fix bugs in initialization

--- disk.c
+++ disk.c
@@ -15,17 +15,17 @@ void set_vol(const char* path){
 //-1 for error 0 for success
 int read_disk(uint64_t block_id, void *buffer)
 {
-    buffer[BLOCKSIZE];
-    int fp = open(volume_paths,O_RDONLY);
-	if (block_id >= BLOCKCOUNT)
-	{
-		printf("block read: %ld exceeded\n",block_id);
-		return -1;
-	}
-    lseek(fp,BLOCKSIZE*block_id,SEEK_SET);
-    read(fp,buffer, BLOCKSIZE);
-    close(fp);
-	return 0;
+        buffer[BLOCKSIZE];
+        int fp = open(volume_paths,O_RDONLY);
+        if (block_id >= BLOCKCOUNT)
+        {
+            printf("block read: %ld exceeded\n",block_id);
+            return -1;
+        }
+        lseek(fp,BLOCKSIZE*block_id,SEEK_SET);
+        read(fp,buffer, BLOCKSIZE);
+        close(fp);
+        return 0;
 }
 
 int write_disk(uint64_t block_id, void *buffer)
--- nodes.c
+++ nodes.c
@@ -133,10 +133,10 @@ void init_i_list()
 void mkfs()
 {
 	char tmp[BLOCKSIZE];
-    read_disk(0,tmp);
     uint64_t *supernode = (uint64_t *)tmp;
     printf("set volume /dev/vdc\n");
     set_vol("/dev/vdc");
+    read_disk(0,tmp);
     if(supernode[3] != 1) {
         printf("no system mounted\nmounting fs...\n");
         memset(tmp, 0, sizeof(tmp));
